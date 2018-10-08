012e170b455f52a15aa1ae6ef8b5c31f0911fcb6
Unknown <t6tujo00@students.oamk.fi>
2018-10-08 13:34:51 +0300

    Käyttöliittymätiedostojen lisääminen
    MainMenuActivity lisätty ja drawableen laitettu taustakuva sekä layouttiin MainMenuActivity.xml

---
+++ app/src/main/java/plan/app/bluetooth/MainMenuActivity.java
@@ -0,0 +1,57 @@
+package e.jope.pingponguserinterface;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.about:
+                //startActivity(new Intent(this, About.class));
+                Toast.makeText(MainMenuActivity.this, "About clicked",
+                        Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.help:
+                //startActivity(new Intent(this, Help.class));
+                Toast.makeText(MainMenuActivity.this, "Help clicked",
+                        Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void connectClicked(View v){
+        Intent myIntent = new Intent(MainMenuActivity.this, connectActivity.class);
+        MainMenuActivity.this.startActivity(myIntent);
+    }
+
+    public void exit(View v){
+        finish();
+        System.exit(0);
+    }
+
+
+
+}
